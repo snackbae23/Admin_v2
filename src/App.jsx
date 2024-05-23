d74559a2a410061f2995a17149c9699390edf258
tanmay2809 <tanmay2809@outlook.com>
2024-05-23 17:05:44 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className=' bg-green-600 '>kjujbbjb</div>
+      <p className='bg-slate-300'>jkjkh</p>
+    </>
+  )
+}
+
+export default App
